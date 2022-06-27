cea0bfbcafe61ec98f0b932c4ba53f69aa6c738f
ReNayeem <renayeem@gmail.com>
2022-06-27 17:09:50 +0600

    Setup done

--- README.md
+++ README.md
@@ -9,3 +9,4 @@ front-end
 npx create-react-app ./
 npm install -D tailwindcss postcss autoprefixer
 npx tailwindcss init -p
+npm i @sanity/client @sanity/image-url react-icons react-loader-spinner react-masonry-css react-router-dom uui react-google-login --force
--- front-end/src/index.js
+++ front-end/src/index.js
@@ -1,7 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { BrowserRouter as Router } from 'react-router-dom'
 
 import App from './App'
 import './index.css'
 
-ReactDOM.render(<App></App>, document.getElementById('root'))
+ReactDOM.render(
+    <Router>
+        <App></App>
+    </Router>
+    , document.getElementById('root'))
